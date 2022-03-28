7e69c2ee0811bf37205215bbe70f063a899ccf2e
kayleen <8iscogb9@anonaddy.me>
2022-03-28 13:15:33 -0500

    initial commit

---
+++ delete.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO animals ( name, type, age ) 
+-- VALUES ('Leo', 'lion', 12),
+-- ('Jerry', 'mouse', 4),
+-- ('Marty', 'zebra', 10),
+-- ('Gloria', 'hippo', 8),
+-- ('Alex', 'lion', 9),
+-- ('Melman', 'giraffe', 15),
+-- ('Nala', 'lion', 2),
+-- ('Marie', 'cat', 1),
+-- ('Flounder', 'fish', 8);
